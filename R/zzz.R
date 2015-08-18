87badfdc93f786a22289d5d5910d6108191c5b9a
ttriche <tim.triche@gmail.com>
2015-08-18 11:59:26 -0700

    sleuth data package, per example

---
+++ R/zzz.R
@@ -0,0 +1,2 @@
+cat("Results from GSE37704 are available in", 
+    system.file("extdata", "results", package="sleuthData"))
---
+++ README.md
@@ -0,0 +1,3 @@
+# sleuthData
+
+HDF5 files from GSE37704 for sleuth testing & examples  
